e7d9b09875ff9ebdb7cc1b1bd41de7405908356d
J0CKA <stefanovic.joja.2004@gmail.com>
2026-06-08 15:10:46 +0200

    Dodate deklaracije DFS u OurCFG-u

---
+++ OurCFG.cpp
@@ -0,0 +1,92 @@
+#include "OurCFG.h"
+
+OurCFG::OurCFG(Function &F)
+{
+  FunctionName = F.getName().str();
+  CreateCFG(F);
+}
+
+void OurCFG::CreateCFG(llvm::Function &F)
+{
+  for ( BasicBlock &BB : F) {
+    AdjacencyList[&BB] = {};
+    for ( BasicBlock *Successor : successors(&BB)) {
+        AdjacencyList[&BB].push_back(Successor);
+    }
+
+//    for (auto &Instr : BB) {
+//      if ( BranchInst *BranchInstr = dyn_cast<BranchInst>(&Instr)) {
+//        AdjacencyList[&BB].push_back(BranchInstr->getSuccessor(0));
+//        if (BranchInstr->isConditional()) {
+//          AdjacencyList[&BB].push_back(BranchInstr->getSuccessor(1));
+//        }
+//      }
+//    }
+  }
+}
+
+void OurCFG::DFS(llvm::BasicBlock *CurrentBlock) 
+{
+  Visited.insert(CurrentBlock);
+
+  for (BasicBlock *Successor : AdjacencyList[CurrentBlock]) {
+    if (Visited.find(Successor) == Visited.end()) {
+      DFS(Successor);
+    }
+  }
+}
+
+bool OurCFG::isReachable(llvm::BasicBlock *BB)
+{
+  return Visited.find(BB) != Visited.end();
+}
+
+void OurCFG::DumpToFile()
+{
+  std::error_code error;
+  raw_fd_stream File("our" + FunctionName + ".dot", error);
+
+
+  File << "digraph \"CFG for '" << FunctionName << "' function\" {\n";
+  File << "\tlabel=\"Ovo je neki nas tekst\";\n\n";
+
+  for ( auto &p : AdjacencyList) {
+    DumpBasicBlock(p.first, File);
+  }
+
+  File << "}\n";
+}
+
+//void OurCFG::DumpBasicBlock( llvm::BasicBlock *Current, llvm::raw_fd_stream &File)
+//{
+//  bool hasMultipleSuccessors = false;
+//
+//  File << "\tNode" << Current << " [shape=record,color=\"#b70d28ff\", style=filled, fillcolor=\"#b70d2870\",label=\"{";
+//
+//  for ( Instruction &Instr : *Current) {
+//    if (Instr.isTerminator()) {
+//      if (BranchInst *BrInstr = dyn_cast<BranchInst>(&Instr)) {
+//        if (BrInstr->isConditional()) {
+//        hasMultipleSuccessors = true;
+//        File << Instr << "\\l|{<s0>True|<s1>False}}\"];\n";
+//      }
+//      }
+//      else {
+//        File << Instr << "\\l}\"];\n";
+//      }
+//    }
+//    else {
+//      File << Instr << "\\l ";
+//    }
+//  }
+//
+//  int successorNum = 0;
+//  for ( BasicBlock *Successor : AdjacencyList[Current]) {
+//    if (hasMultipleSuccessors) {
+//      File << "\tNode" << Current << ":s" << successorNum++ << " -> Node" << Successor << ";\n";
+//    }
+//    else {
+//      File << "\tNode" << Current << " -> Node" << Successor << ";\n";
+//    }
+//  }
+//}
