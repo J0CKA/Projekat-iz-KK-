f27375fa0c1299ef29e7e24f61a8156184f72525
J0CKA <stefanovic.joja.2004@gmail.com>
2026-06-08 15:03:09 +0200

    Dodata OurCFG datoteka zaglavlja klasa

---
+++ OurCFG.h
@@ -0,0 +1,27 @@
+#ifndef LLVM_PROJECT_OURCFG_H
+#define LLVM_PROJECT_OURCFG_H
+
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+using namespace llvm;
+
+class OurCFG {
+private:
+  std::string FunctionName;
+  std::unordered_set<BasicBlock *>Visited;
+  std::unordered_map<BasicBlock *, std::vector<BasicBlock *>> AdjacencyList;
+  void CreateCFG(Function &F);
+  void DumpBasicBlock(BasicBlock *, raw_fd_stream &);
+public:
+  OurCFG(Function &F);
+  void DFS(BasicBlock *); 
+  bool isReachable(BasicBlock *); 
+  void DumpToFile();
+};
+
+#endif 
