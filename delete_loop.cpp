84f88b595a7e43f8d99360f7bd28b3fe23a81c29
J0CKA <stefanovic.joja.2004@gmail.com>
2026-06-13 00:23:07 +0200

    dodati komentati

--- delete_loop.cpp
+++ delete_loop.cpp
@@ -1,3 +1,8 @@
+/*Ovaj pass traži petlje koje:
+-nemaju bočne efekte (ne menjaju memoriju / ne rade I/O / ne zovu funkcije)
+-ne koriste se van petlje
+-imaju poznat broj iteracija*/
+
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
@@ -18,54 +23,54 @@
 
 using namespace llvm;
 
-namespace {
+namespace {// pravimo anonimni namespace da se klase ne sudaraju sa drugim pass-evima
 
 struct BrisanjeMrtvihPetljiPass
-    : public PassInfoMixin<BrisanjeMrtvihPetljiPass> {
+    : public PassInfoMixin<BrisanjeMrtvihPetljiPass> { //definicija LLVM pass-a
 
-    bool ImaBocneEfekte(Loop *Petlja) {
-        for (BasicBlock *BB : Petlja->blocks()) {
-            for (Instruction &Inst : *BB) {
+    bool ImaBocneEfekte(Loop *Petlja) { // provera da li petlja sadrzi bocne efekte
+        for (BasicBlock *BB : Petlja->blocks()) { //prolazimo kroz sve basic blokove u ovoj petlji
+            for (Instruction &Inst : *BB) { //prolazimo kroz sve instrukcije u svakom bloku
 
-                if (Inst.mayHaveSideEffects())
+                if (Inst.mayHaveSideEffects()) // ako instrukcija ima efekte -> NE SME SE BRISATI
                     return true;
 
-                if (isa<CallBase>(&Inst))
+                if (isa<CallBase>(&Inst)) //ako je funkcijski poziv -> POTENCIJALNI BOCNI EFEKAT
                     return true;
             }
         }
 
-        return false;
+        return false; // petlja je CISTA 
     }
 
-    bool KoristiSeVanPetlje(Loop *Petlja) {
-        
+    bool KoristiSeVanPetlje(Loop *Petlja) { // Sakupimo sve legitimne izlazne blokove ove petlje
         SmallVector<BasicBlock *, 4> IzlazniBlokovi;
-        Petlja->getExitBlocks(IzlazniBlokovi);
+        Petlja->getExitBlocks(IzlazniBlokovi); //uzimamo samo blokove gde se izlazi iz petlje
 
         for (BasicBlock *BB : Petlja->blocks()) {
             for (Instruction &Inst : *BB) {
-                for (User *U : Inst.users()) {
-                    if (auto *KorisnikInst = dyn_cast<Instruction>(U)) {
+                for (User *U : Inst.users()) { //gledamo ko koristi rezultat instrukcije
+                    if (auto *KorisnikInst = dyn_cast<Instruction>(U)) { //proveravamo da li je korisnik instrukcija
                         
-                        
+                        // Ako je korisnik van petlje
                         if (!Petlja->contains(KorisnikInst)) {
                             
-                           
+                            // Ako je taj korisnik PHI čvor u nekom od izlaznih blokova petlje,
+                            // to je bezbedno za brisanje jer LLVM to ume automatski da sredi!
                             if (auto *PN = dyn_cast<PHINode>(KorisnikInst)) {
                                 bool JeIzlazniPHI = false;
-                                for (BasicBlock *EB : IzlazniBlokovi) {
+                                for (BasicBlock *EB : IzlazniBlokovi) { 
                                     if (PN->getParent() == EB) {
-                                        JeIzlazniPHI = true;
+                                        JeIzlazniPHI = true; //izlazni PHI se ne računa kao “problem” -> IGNORISE SE
                                         break;
                                     }
                                 }
                                 if (JeIzlazniPHI) {
-                                    continue; 
+                                    continue; // Preskačemo, ovo nam ne blokira brisanje petlje
                                 }
                             }
                             
-                            
+                            // U svakom drugom slučaju, vrednost stvarno beži van i NE SMEMO BRISATI
                             return true;
                         }
                     }
@@ -76,47 +81,44 @@ struct BrisanjeMrtvihPetljiPass
         return false;
     }
 
-    PreservedAnalyses run(Function &F,
-                          FunctionAnalysisManager &FAM) {
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) { //ovo llvm poziva za svaku funkciju
 
-        LoopInfo &LI =
-            FAM.getResult<LoopAnalysis>(F);
+        LoopInfo &LI = FAM.getResult<LoopAnalysis>(F);
+        // LI -> gde su petlje
 
-        ScalarEvolution &SE =
-            FAM.getResult<ScalarEvolutionAnalysis>(F);
+        ScalarEvolution &SE = FAM.getResult<ScalarEvolutionAnalysis>(F);
+        // SE -> koliko puta se izvršavaju
 
-        DominatorTree &DT =
-            FAM.getResult<DominatorTreeAnalysis>(F);
+        DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+        // DT -> dominacija blokova (za bezbedno brisanje)
 
         bool Izmenjeno = false;
 
-        std::vector<Loop *> MrtvePetlje;
+        std::vector<Loop *> MrtvePetlje; // lista petlji koje će biti obrisane
 
-        for (Loop *Petlja : LI) {
+        for (Loop *Petlja : LI) {// sve petlje u funkciji
 
             if (!Petlja->getSubLoops().empty())
-                continue;
+                continue; // preskacemo ugnjezdene petlje -- samo najunutrasnjije obradjujemo
 
             const SCEV *BrojIteracija =
-                SE.getBackedgeTakenCount(Petlja);
+                SE.getBackedgeTakenCount(Petlja); // racunamo broj iteracija petlje
 
             if (isa<SCEVCouldNotCompute>(BrojIteracija))
-                continue;
+                continue; // ako ne moze da izracuna -> NE DIRAJ PETLJU
 
             if (ImaBocneEfekte(Petlja))
-                continue;
+                continue; // ako ima bocne efekte -> NE BRISI 
 
             if (KoristiSeVanPetlje(Petlja))
-                continue;
+                continue; // ako rezultat izlazi iz petlje -> NE BRISI
 
-            MrtvePetlje.push_back(Petlja);
+            MrtvePetlje.push_back(Petlja); //ako je mrtva -> KANDIDAT ZA BRISANJE
         }
 
-        for (Loop *Petlja : MrtvePetlje) {
+        for (Loop *Petlja : MrtvePetlje) { // prolazimo kroz mrtve petlje
 
-            errs() << "[LoopDeletion] Pronadjena i brise se mrtva petlja!\n";
-
-            
+            // LLVM funkcija koja uklanja petlju iz CFG-a, ažurira dominatore i loop info i uklanja blokove
             deleteDeadLoop(
                 Petlja,
                 &DT,
@@ -124,17 +126,17 @@ struct BrisanjeMrtvihPetljiPass
                 &LI
             );
 
-            Izmenjeno = true;
+            Izmenjeno = true; //oznacavamo da je doslo do promene
         }
 
         if (Izmenjeno)
-            return PreservedAnalyses::none();
+            return PreservedAnalyses::none(); //ako je nešto obrisano -> analiza više nije validna
 
-        return PreservedAnalyses::all();
+        return PreservedAnalyses::all();// ako ništa nije promenjeno -> sve analize ostaju validne
     }
 };
 
-} 
+} // namespace
 
 llvm::PassPluginLibraryInfo
 getBrisanjeMrtvihPetljiPluginInfo() {
@@ -145,7 +147,7 @@ getBrisanjeMrtvihPetljiPluginInfo() {
 
         [](PassBuilder &PB) {
 
-            PB.registerPipelineParsingCallback(
+            PB.registerPipelineParsingCallback( //omogućava da se pass zove iz CLI-a
                 [](StringRef Name,
                    FunctionPassManager &FPM,
                    ArrayRef<PassBuilder::PipelineElement>) {
@@ -164,10 +166,11 @@ getBrisanjeMrtvihPetljiPluginInfo() {
     };
 }
 
+//LLVM traži ovu funkciju kada učita plugin
 extern "C"
 LLVM_ATTRIBUTE_WEAK
 ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
 
-    return getBrisanjeMrtvihPetljiPluginInfo();
+    return getBrisanjeMrtvihPetljiPluginInfo(); //vraća definiciju plugina
 }
