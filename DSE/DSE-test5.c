65ce63169def5b2688e087e1c04d813901d154e1
minam28 <73477911+minam28@users.noreply.github.com>
2026-06-14 15:44:49 +0200

    Create DSE-test5.c

---
+++ DSE/DSE-test5.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+int main_test(bool cond) {
+    int x;
+    x = 5;   //mrtav
+    
+    if (cond) {
+        // then:
+        x = 10;  //živ ako je uslov tačan
+    } else {
+        x = 20;  //živ ako je uslov netačan
+    }
+
+    int v = x; 
+    return v;
+}
