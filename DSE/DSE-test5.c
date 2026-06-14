7dc2a51e3b818c624e6e46ca21e297e7a6253cb5
minam28 <73477911+minam28@users.noreply.github.com>
2026-06-14 16:13:42 +0200

    Update DSE-test5.c

--- DSE/DSE-test5.c
+++ DSE/DSE-test5.c
@@ -2,10 +2,9 @@
 
 int main_test(bool cond) {
     int x;
-    x = 5;   //mrtav
+    x = 5;  //mrtav
     
     if (cond) {
-        // then:
         x = 10;  //živ ako je uslov tačan
     } else {
         x = 20;  //živ ako je uslov netačan
