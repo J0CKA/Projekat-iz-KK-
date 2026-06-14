f510270b64110f8d86e51f8f409e303c092e4b0e
J0CKA <stefanovic.joja.2004@gmail.com>
2026-06-14 15:51:38 +0200

    primer

---
+++ DSE/DSE-test2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+volatile int sink;
+
+void napredni_test(int *p, int *q, int uslov)
+{
+    *p = 100;      // mrtav
+
+    *q = 5;        // mora da ostane
+
+    if (uslov)
+        sink = *q;
+
+    *p = 200;
+}
+
+int main(void)
+{
+    int a = 0;
+    int b = 0;
+
+    napredni_test(&a, &b, 1);
+
+    return 0;
+}
