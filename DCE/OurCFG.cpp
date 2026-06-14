652e5721d3fcc95d32729dfdb79a4c98b0febf53
minam28 <73477911+minam28@users.noreply.github.com>
2026-06-14 19:44:30 +0200

    komentari OurCFG.cpp

--- DCE/OurCFG.cpp
+++ DCE/OurCFG.cpp
@@ -1,19 +1,22 @@
 #include "OurCFG.h"
 
+//konstruktor klase - inicijalizuje CFG
 OurCFG::OurCFG(Function &F)
 {
-  FunctionName = F.getName().str();
-  CreateCFG(F);
+  FunctionName = F.getName().str(); //čuva kao string ime fje
+  CreateCFG(F); //gradi graf
 }
 
+//konstruiše graf na osnovu strukture funkcije
 void OurCFG::CreateCFG(llvm::Function &F)
 {
-  for ( BasicBlock &BB : F) {
-    AdjacencyList[&BB] = {};
+  for ( BasicBlock &BB : F) { //prolazi kroz sve bazne blokove unutar funkcije
+    AdjacencyList[&BB] = {}; //inicijalizuje praznu listu naslednika za dati blok
     for ( BasicBlock *Successor : successors(&BB)) {
-        AdjacencyList[&BB].push_back(Successor);
+        AdjacencyList[&BB].push_back(Successor); //dodaje naslednika
     }
 
+    //STARI NAČIN 
 //    for (auto &Instr : BB) {
 //      if ( BranchInst *BranchInstr = dyn_cast<BranchInst>(&Instr)) {
 //        AdjacencyList[&BB].push_back(BranchInstr->getSuccessor(0));
@@ -24,23 +27,25 @@ void OurCFG::CreateCFG(llvm::Function &F)
 //    }
   }
 }
-
-void OurCFG::DFS(llvm::BasicBlock *CurrentBlock) 
+//rekurzivni obilazak grafa u dubinu počevši od zadatog bloka
+void OurCFG::DFS(llvm::BasicBlock *CurrentBlock)
 {
-  Visited.insert(CurrentBlock);
+  Visited.insert(CurrentBlock); //trenutni blok označava kao posećen ubacivanjem u kup
 
   for (BasicBlock *Successor : AdjacencyList[CurrentBlock]) {
-    if (Visited.find(Successor) == Visited.end()) {
+    if (Visited.find(Successor) == Visited.end()) { //ako naslednik još uvek nije posećen, rekurzivno pokreće DFS za njega
       DFS(Successor);
     }
   }
 }
 
+//proverava da li je zadati bazni blok dostižan (da li ga je DFS posetio)
 bool OurCFG::isReachable(llvm::BasicBlock *BB)
 {
   return Visited.find(BB) != Visited.end();
 }
 
+//generiše .dot fajl koji služi za vizuelizaciju grafa pomoću Graphviz alata
 void OurCFG::DumpToFile()
 {
   std::error_code error;
